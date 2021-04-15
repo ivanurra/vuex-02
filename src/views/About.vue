36473e278b4557d8cd253c3aea306d6468ea4bb7
ivanurra <ivan.urra.wd@gmail.com>
2021-04-15 17:23:35 +0200

    v-model

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,17 @@
   <div class="about">
     <h2>About page</h2>
     <h3>Counter: {{ $store.state.counter }} </h3>
+    <input type="text" v-model="text">
+    <h3>{{ text }}</h3>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  },
+}
+</script>
