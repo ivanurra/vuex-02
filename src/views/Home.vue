fee4fde9d670ac5c54998cc4763e279513af5374
ivanurra <ivan.urra.wd@gmail.com>
2021-04-15 13:07:15 +0200

    mapState

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="home">
-    <h3>Counter: {{ $store.state.counter }} </h3>
+    <!-- <h3>Counter: {{ $store.state.counter }}</h3> -->
+    <h3>Counter: {{ counter }}</h3>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 
 export default {
   name: 'Home',
   components: {
 
+  },
+  computed: {
+    ...mapState([`counter`])
   }
 }
 </script>
